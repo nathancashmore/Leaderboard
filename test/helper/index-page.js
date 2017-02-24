f10910b0e6785606c2665245812e2fe84537c803
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2017-02-24 14:55:29 +0000

    Add sample data to index-controller

--- test/helper/index-page.js
+++ test/helper/index-page.js
@@ -1,8 +1,12 @@
 const IndexPage = function IndexPage(browser) {
   this.browser = browser;
 
-  this.visit = () => this.browser.visit('/leaderboard');
-  this.loadingSectionDisplayed = () => browser.assert.element('#loading');
+  this.visit = () => browser.visit('/leaderboard');
+  this.serverDetails = () => browser.text('[data-test="server-details"]');
+  this.avatar = () => browser.query('[data-test="avatar"]').src;
+  this.player = () => browser.text('[data-test="player"]');
+  this.achievements = () => browser.query('[data-test="achievement"]').className;
+  this.score = () => browser.text('[data-test="score"]');
 };
 
 module.exports = IndexPage;
