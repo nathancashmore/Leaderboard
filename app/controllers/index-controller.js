51c1fc173c5d2081d5c58047a60ee7f39f4f129d
Nathan Cashmore <nathan.cashmore@staticvoid.co.uk>
2017-02-26 22:31:32 +0000

    Change URL to reference configured port

--- app/controllers/index-controller.js
+++ app/controllers/index-controller.js
@@ -4,8 +4,8 @@ const logger = require('winston');
 
 const router = new express.Router();
 
-router.get('/', (req, res) =>
-  request('http://localhost:25566/server/details').then((result) => {
+router.get('/', (req, res) => {
+  request(`http://localhost:${req.app.locals.settings.port}/server/details`).then((result) => {
     const serverDetails = JSON.parse(result);
 
     const pageData = {
@@ -16,8 +16,7 @@ router.get('/', (req, res) =>
     };
 
     res.render('index', pageData);
-  }).catch(e => logger.log('error', e))
-);
-
+  }).catch(e => logger.log('error', e));
+});
 
 module.exports = router;
