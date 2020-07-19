4cc2041cbed9e6847349862902e2b00779e137ac
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-07-19 11:43:58 +0100

    Remove eslinting from es5 based file used in PhantomJS

--- app/middleware/exportLeaderboardAsImage.js
+++ app/middleware/exportLeaderboardAsImage.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console, import/no-unresolved, no-undef, prefer-template */
+/* eslint-disable */
 const system = require('system');
 
 console.log('Script: ' + system.args[0]);
@@ -6,6 +6,7 @@ console.log('Url: ' + system.args[1]);
 console.log('Output: ' + system.args[2]);
 
 const page = require('webpage').create();
+
 page.open(system.args[1], function() {
   page.render(system.args[2]);
   phantom.exit();
