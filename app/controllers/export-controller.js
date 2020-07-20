5f50beb5368c5acd148aab495a9324b173b93022
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-07-20 18:27:30 +0100

    Change export filename

--- app/controllers/export-controller.js
+++ app/controllers/export-controller.js
@@ -8,7 +8,7 @@ const router = new express.Router();
 router.get('/', (req, res) => {
   const script = path.join(__dirname, '../middleware/exportLeaderboardAsImage.js');
   const url = `http://localhost:${req.app.get('port')}/leaderboard`;
-  const filename = `${req.app.locals.exportPath}/export.png`;
+  const filename = `${req.app.locals.exportPath}/leaderboard.png`;
 
   const program = phantomjs.exec(script, url, filename);
 
--- test/integration/export-controller.spec.js
+++ test/integration/export-controller.spec.js
@@ -10,13 +10,9 @@ const file = chaiFiles.file;
 const helper = require('../test-helper');
 
 const exportPage = helper.exportPage;
-const exportFilename = path.join(__dirname, '../../app/data/export.png');
+const exportFilename = path.join(__dirname, '../../app/data/leaderboard.png');
 
 describe('Export Controller - Default', () => {
-  before(() =>
-    helper.removeFile(exportFilename)
-  );
-
   after(() =>
     helper.removeFile(exportFilename)
   );
@@ -26,7 +22,7 @@ describe('Export Controller - Default', () => {
     exportPage.visit()
       .then(() => {
         expect(file(exportFilename)).to.exist;
-        expect(md5(exportPage.body())).to.equal('e4f5ccdc69b690c0c6262af6a5f4cc15');
+        expect(md5(exportPage.body())).to.equal('f9ff56104ba25b7b6591543f3a2177c0');
       })
   ).timeout(8000);
 });
