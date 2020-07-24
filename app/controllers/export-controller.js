546ab31f293bdde8fcf6b99f52eae9951b3e36e3
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-07-24 09:30:12 +0100

    Refactor export-controller to simplify and use async promise structure

--- app/controllers/export-controller.js
+++ app/controllers/export-controller.js
@@ -5,28 +5,33 @@ const path = require('path');
 
 const router = new express.Router();
 
-router.get('/', (req, res) => {
-  const script = path.join(__dirname, '../middleware/exportLeaderboardAsImage.js');
+async function downloadFile(url, filename) {
+  const script = path.join(__dirname, '../middleware/exportPageAsImage.js');
+
+  return new Promise(((resolve, reject) => {
+    const program = phantomjs.exec(script, url, filename);
+
+    program.stdout.pipe(process.stdout);
+    program.stderr.pipe(process.stderr);
+
+    program.on('exit', (code) => {
+      if (code === 0) {
+        logger.log('info', `Success - file written to ${filename}`);
+        resolve();
+      } else {
+        logger.log('error', `Error when attempting to output file : ${code}`);
+        reject();
+      }
+    });
+  }));
+}
+
+router.get('/', async (req, res) => {
   const url = `http://localhost:${req.app.get('port')}/leaderboard`;
   const filename = `${req.app.locals.exportPath}/leaderboard.png`;
 
-  const program = phantomjs.exec(script, url, filename);
-
-  program.stdout.pipe(process.stdout);
-  program.stderr.pipe(process.stderr);
-
-  program.on('exit', (code) => {
-    if (code === 0) {
-      logger.log('info', `Success - file written to ${filename}`);
-    } else {
-      logger.log('error', `Error when attempting to output file : ${code}`);
-    }
-  });
-
-  setTimeout(() => {
-    logger.log('info', 'Sending file');
-    res.download(filename);
-  }, 4000);
+  await downloadFile(url, filename);
+  res.download(filename);
 });
 
 module.exports = router;
