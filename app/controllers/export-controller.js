2f3340036b01daabea5a45d1c2f3ed8efd1bba01
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-07-25 21:48:34 +0100

    Add dependencies

--- app/controllers/export-controller.js
+++ app/controllers/export-controller.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('winston');
 const phantomjs = require('phantomjs-prebuilt');
 const path = require('path');
+const fs = require('fs');
 
 const ServerHelper = require('../util/server-helper');
 
